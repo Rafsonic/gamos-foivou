f8ab7a42b8d8a740ceabce8711f328dc0d1ca89e
rafsonic <raf-ael@hotmail.com>
2025-10-05 20:45:55 +0300

    languages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import MobileMenu from "../MobileMenu/MobileMenu";
 import { useTranslation } from "react-i18next";
+import en from "./../../images/lng/uk.png";
+import gr from "./../../images/lng/gr.png";
 
 const Header = (props) => {
   const [menuActive, setMenuState] = useState(false);
@@ -14,12 +16,29 @@ const Header = (props) => {
     window.scrollTo(10, 0);
   };
 
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const lang = i18n.language;
+  const isGreek = lang.toLowerCase().includes("gr");
+  const changeLang = (lng) => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <header id="header" className={props.topbarNone}>
       <div className={`wpo-site-header ${props.hclass}`}>
-        <nav className="navigation navbar navbar-expand-lg navbar-light">
+        <nav className="navigation navbar navbar-expand-lg navbar-light kolitos relative">
+          <div id="lng-picker">
+            {isGreek ? (
+              <button onClick={() => changeLang("en")}>
+                <img height={25} src={en} alt="en"></img>
+              </button>
+            ) : (
+              <button onClick={() => changeLang("gr")}>
+                <img height={25} src={gr} alt="gr"></img>
+              </button>
+            )}
+          </div>
           <div className="container-fluid">
             <div className="row align-items-center">
               <div className="col-lg-3 col-md-3 col-3 d-lg-none dl-block">
